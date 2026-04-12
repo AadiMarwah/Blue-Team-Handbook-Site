2cd95ade2bcca246edc171c36a1f70c7ae64fa67
AadiMarwah <aadimarwah316@gmail.com>
2026-04-13 04:01:52 +0530

    Quartz sync: Apr 13, 2026, 4:01 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -22,12 +22,9 @@ export const sharedPageComponents: SharedLayout = {
     }
   }),
 ],
-  footer: Component.Footer({
-    links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
-    },
-  }),
+  footer: Component.Footer(
+    
+  ),
 }
 
 // components for pages that display a single page (e.g. a single note)
